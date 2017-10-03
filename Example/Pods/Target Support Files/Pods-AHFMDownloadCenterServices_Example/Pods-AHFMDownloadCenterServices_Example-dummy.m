84f3eaa8f7dd8289cf9518b60e0039e0512d0a5b
iOSModularization <Hurricane@Awesome-Sht.local>
2017-10-02 17:55:09 -0700

    ok

---
+++ Example/Pods/Target Support Files/Pods-AHFMDownloadCenterServices_Example/Pods-AHFMDownloadCenterServices_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AHFMDownloadCenterServices_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AHFMDownloadCenterServices_Example
+@end
